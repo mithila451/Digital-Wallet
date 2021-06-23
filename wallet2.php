a3959d85f726ccd12256d4a68f7ed9151d649145
mithila451 <79931351+mithila451@users.noreply.github.com>
2021-06-23 14:50:37 +0600

    Add files via upload

---
+++ wallet2.php
@@ -0,0 +1,35 @@
+<html>
+<head><title> First Page </title>  </head>
+<body>
+<fieldset>
+ <font color="red"><h2><b>Page 2 [Transaction History]</b></h1></font> 
+<font color="red"><h2>Digital Wallet</h1></font>
+ <a href="Page1.php">1.Home</a>
+ <a href="Page2.php">2.Transaction History</a>
+ 
+  
+ <font color="red"><h2>Total Records:(4)</h1></font> 
+ <fieldset>
+ <table style="width:100%">
+ <tr>
+ <th>Transaction Catetgory</th>
+ <td>To</td>
+ <td> Amount</td>
+ <td> Transferred On</td>
+
+ </tr>
+ <tr>
+    <th rowspan="3">mobile_recharge:
+    </th>
+
+     <td>01684999409</td>
+     </tr>
+     <tr>
+     <td>01715675206</td>
+     </tr>
+     <tr>
+     
+     <td>019226963</td>
+     </tr>
+      <tr>
+    </table>
